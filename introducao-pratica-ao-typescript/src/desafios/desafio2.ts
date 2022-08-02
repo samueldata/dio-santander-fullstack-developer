c3cd3df4d91a2d8ec85651deffd74a8e86fc4721
samanolo <datasamuel@outlook.com>
2022-08-02 08:36:53 -0300

    updating progress

--- introducao-pratica-ao-typescript/src/desafios/desafio2.ts
+++ introducao-pratica-ao-typescript/src/desafios/desafio2.ts
@@ -1,4 +1,28 @@
 // Como podemos melhorar esse código usando TS? 
+
+// let pessoa1 = {};
+// pessoa1.nome = "maria";
+// pessoa1.idade = 29;
+// pessoa1.profissao = "atriz"
+
+// let pessoa2 = {}
+// pessoa2.nome = "roberto";
+// pessoa2.idade = 19;
+// pessoa2.profissao = "Padeiro";
+
+// let pessoa3 = {
+//     nome: "laura",
+//     idade: "32",
+//     profissao: "Atriz"
+// };
+
+// let pessoa4 = {
+//     nome = "carlos",
+//     idade = 19,
+//     profissao = "padeiro"
+// }
+
+// Resposta
 interface Pessoa {
     nome: string, 
     idade: number, 
