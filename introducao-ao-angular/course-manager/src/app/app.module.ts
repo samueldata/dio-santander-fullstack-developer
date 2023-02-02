54c0b13737b91e40aa6c9760caf175a10dee5185
samanolo <datasamuel@outlook.com>
2023-02-01 23:44:21 -0300

    remote repository sync

--- introducao-ao-angular/course-manager/src/app/app.module.ts
+++ introducao-ao-angular/course-manager/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.components';
+import { CourseListComponent } from './courses/course-list.component';
 
 @NgModule({
   declarations: [
